bd9c3c4756a0dccac5c85439bab3852f4e6825b2
Raghu Betina <raghu@firstdraft.com>
2018-04-12 05:09:56 +0000

    Direct association between dishes and venues

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,10 @@
 class Dish < ApplicationRecord
   # Direct associations
 
+  has_many   :venues,
+             :foreign_key => "dishes_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
