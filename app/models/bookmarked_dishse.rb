a416a3f1227c8bd1a7c5feb27e5f590e9a0a9144
Raghu Betina <raghu@firstdraft.com>
2018-04-12 05:09:56 +0000

    Direct association between venues and bookmarked_dishses

--- app/models/bookmarked_dishse.rb
+++ app/models/bookmarked_dishse.rb
@@ -1,6 +1,9 @@
 class BookmarkedDishse < ApplicationRecord
   # Direct associations
 
+  belongs_to :venues,
+             :class_name => "Venue"
+
   # Indirect associations
 
   # Validations
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,6 +1,11 @@
 class Venue < ApplicationRecord
   # Direct associations
 
+  has_many   :bookmarkeds,
+             :class_name => "BookmarkedDishse",
+             :foreign_key => "venues_id",
+             :dependent => :destroy
+
   belongs_to :dishes,
              :class_name => "Dish"
 
