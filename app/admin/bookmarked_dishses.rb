4a663100f3b13f0f874596a0dac0fddee146ba44
Raghu Betina <raghu@firstdraft.com>
2018-04-12 05:10:29 +0000

    Whitelist columns in BookmarkedDishse for ActiveAdmin

--- app/admin/bookmarked_dishses.rb
+++ app/admin/bookmarked_dishses.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register BookmarkedDishse do
+
+ permit_params :venues_id, :users_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
